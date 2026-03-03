0e7857a41879f42ecc886e01485614a39111b014
Newton Ongeri <ongerinewton10@gmail.com>
2026-03-03 17:52:31 +0300

    import and render projects page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
+import Projects from "./components/Projects";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Hero />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
