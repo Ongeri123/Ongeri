ec9f9e43609781875c8bccf0e4210fd8bbb66384
Newton Ongeri <ongerinewton10@gmail.com>
2026-03-04 18:43:52 +0300

    import and render stack page to the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 import Projects from "./components/Projects";
+import Stack from "./components/Stack";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/stack" element={<Stack />} />
       </Routes>
     </BrowserRouter>
   );
